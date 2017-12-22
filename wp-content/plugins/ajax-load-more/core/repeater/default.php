bac8b042446cd4c6c8562919fb3b729075c858a0
vdthanh <vdthanh@tma.com.vn>
2017-12-22 17:37:33 +0700

    Chang layout for product summary page

---
+++ wp-content/plugins/ajax-load-more/core/repeater/default.php
@@ -0,0 +1,10 @@
+<ul>
+     <li>
+          <h3>
+               <a href="<?php the_permalink(); ?>">
+               <?php the_title(); ?>
+               </a>
+          </h3>
+          <?php the_post_thumbnail(); ?>          
+     </li>     
+</ul>
