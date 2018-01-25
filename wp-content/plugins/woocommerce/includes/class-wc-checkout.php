9ff1583ea2abcdf0f6f2bbfe4802ccfa5bb49035
Michaelrau <thanhvd84@gmail.com>
2018-01-25 21:30:07 +0700

    Update code based on latest comment

--- wp-content/plugins/woocommerce/includes/class-wc-checkout.php
+++ wp-content/plugins/woocommerce/includes/class-wc-checkout.php
@@ -697,23 +697,23 @@ class WC_Checkout {
 			$errors->add( 'terms', __( 'You must accept our Terms &amp; Conditions.', 'woocommerce' ) );
 		}
 
-		if ( WC()->cart->needs_shipping() ) {
-			$shipping_country = WC()->customer->get_shipping_country();
+		// if ( WC()->cart->needs_shipping() ) {
+		// 	$shipping_country = WC()->customer->get_shipping_country();
 
-			if ( empty( $shipping_country ) ) {
-				$errors->add( 'shipping', __( 'Please enter an address to continue.', 'woocommerce' ) );
-			} elseif ( ! in_array( WC()->customer->get_shipping_country(), array_keys( WC()->countries->get_shipping_countries() ) ) ) {
-				$errors->add( 'shipping', sprintf( __( 'Unfortunately <strong>we do not ship %s</strong>. Please enter an alternative shipping address.', 'woocommerce' ), WC()->countries->shipping_to_prefix() . ' ' . WC()->customer->get_shipping_country() ) );
-			} else {
-				$chosen_shipping_methods = WC()->session->get( 'chosen_shipping_methods' );
+		// 	if ( empty( $shipping_country ) ) {
+		// 		$errors->add( 'shipping', __( 'Please enter an address to continue.', 'woocommerce' ) );
+		// 	} elseif ( ! in_array( WC()->customer->get_shipping_country(), array_keys( WC()->countries->get_shipping_countries() ) ) ) {
+		// 		$errors->add( 'shipping', sprintf( __( 'Unfortunately <strong>we do not ship %s</strong>. Please enter an alternative shipping address.', 'woocommerce' ), WC()->countries->shipping_to_prefix() . ' ' . WC()->customer->get_shipping_country() ) );
+		// 	} else {
+		// 		$chosen_shipping_methods = WC()->session->get( 'chosen_shipping_methods' );
 
-				foreach ( WC()->shipping->get_packages() as $i => $package ) {
-					if ( ! isset( $chosen_shipping_methods[ $i ], $package['rates'][ $chosen_shipping_methods[ $i ] ] ) ) {
-						$errors->add( 'shipping', __( 'No shipping method has been selected. Please double check your address, or contact us if you need any help.', 'woocommerce' ) );
-					}
-				}
-			}
-		}
+		// 		foreach ( WC()->shipping->get_packages() as $i => $package ) {
+		// 			if ( ! isset( $chosen_shipping_methods[ $i ], $package['rates'][ $chosen_shipping_methods[ $i ] ] ) ) {
+		// 				$errors->add( 'shipping', __( 'No shipping method has been selected. Please double check your address, or contact us if you need any help.', 'woocommerce' ) );
+		// 			}
+		// 		}
+		// 	}
+		// }
 
 		if ( WC()->cart->needs_payment() ) {
 			$available_gateways = WC()->payment_gateways->get_available_payment_gateways();
